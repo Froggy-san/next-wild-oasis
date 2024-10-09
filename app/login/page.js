b6d790248066f719d46a93505f52b9300caa665c
Froggy-san <medoosama441@yahoo.com>
2024-10-09 16:14:33 +0300

    trying

--- app/login/page.js
+++ app/login/page.js
@@ -3,6 +3,7 @@ export const metadata = {
   title: "Login",
 };
 export default function Page() {
+  console.log("");
   return (
     <div className="flex flex-col gap-10 mt-10 items-center">
       <h2 className="text-3xl font-semibold">
